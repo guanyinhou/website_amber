d1feee15895864d1cc869940b67941589f1cac07
guanyinhou <moonightriver@gmail.com>
2020-09-07 15:39:12 +0800

    update works filter

--- src/views/front/Order.vue
+++ src/views/front/Order.vue
@@ -1,8 +1,126 @@
 <template>
   <div class="order">
+    <loading :active.sync="isLoading"></loading>
     <div class="container">
       <h1>訂單查詢</h1>
       <hr />
+      <div class="text-center">
+        <form class="form-signin" @submit.prevent.enter="searchOrder(order)">
+          <br />
+          <br />
+          <br />
+          <br />
+          <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+              <!-- <input
+                type="email"
+                class="form-control"
+                placeholder="E-mail"
+                v-model="order.email"
+                required
+              />
+              <br /> -->
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Order Number"
+                v-model="order.id"
+                required
+              />
+            </div>
+            <div class="col-sm-4"></div>
+          </div>
+          <br />
+          <br />
+          <br />
+          <div class="btn-zone">
+            <button class="btn" type="submit">送出查詢</button>
+            <br />
+          </div>
+        </form>
+        <div class="order-result" :class="{ active: resultShow }">
+          <br />
+          <br />
+          <br />
+          <hr />
+          <br />
+          <table class="table-hover table-bordered">
+            <thead>
+              <tr>
+                <th>訂單編號</th>
+                <th>訂單日期</th>
+                <th>訂單金額</th>
+                <th>付款狀態</th>
+                <th>購買內容</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{{ result.id }}</td>
+                <td>{{ result.created.datetime }}</td>
+                <td>{{ result.amount }}</td>
+                <td>{{ result.paid }}</td>
+                <td>
+                  <a href="">詳細</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoading: false,
+      order: {
+        email: "",
+        id: ""
+      },
+      orders: {},
+      result: {
+        created: {
+          datetime: ""
+        }
+      },
+      resultShow: false
+    };
+  },
+  // created() {
+  //   this.searchOrder();
+  // },
+  methods: {
+    searchOrder(order) {
+      // this.datetime = this.result.created.datetime;
+      this.isLoading = true;
+      // const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_UUID}/ec/orders`;
+      const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_UUID}/ec/orders/${order.id}`;
+      console.log(url);
+      // if (order.email === orders[]) {
+      this.$http
+        .get(url)
+        .then(res => {
+          this.isLoading = false;
+          this.result = res.data.data;
+          console.log(this.result);
+          // order.forEach(ele => {
+          //   console.log(ele);
+          // });
+          // this.$router.push(`./order/${order.id}`);
+          this.order.id = "";
+          this.resultShow = true;
+        })
+        .catch(err => {
+          this.isLoading = false;
+          console.dir(err);
+        });
+      // }
+    }
+  }
+};
+</script>
--- src/views/front/Works.vue
+++ src/views/front/Works.vue
@@ -4,34 +4,48 @@
       <h1>作品集</h1>
       <hr />
       <br />
-      <ul class="row prod-list">
-        <li v-for="prod in prods" :key="prod.id" class="col-sm-3">
-          <router-link :to="`/product/${prod.id}`">
-            <div class="prod-image">
-              <img :src="prod.imageUrl[0]" :alt="prod.title" />
-            </div>
-            <div class="prod-title">{{ prod.title }}</div>
-            <div
-              class="prod-origin-price"
-              v-if="prod.origin_price === prod.price"
-            >
-              售價&nbsp;&nbsp;&nbsp;&nbsp;{{ prod.origin_price | currency }}
-            </div>
-            <span v-else>
-              <div class="prod-origin-price old-price">
-                售價&nbsp;&nbsp;&nbsp;&nbsp;
-                {{ prod.origin_price | currency }}
-              </div>
-              <div class="prod-price">
-                <b>特價&nbsp;&nbsp;&nbsp;&nbsp;{{ prod.price | currency }}</b>
-              </div>
-            </span>
-          </router-link>
-          <a href="#" class="add-to-cart" @click.prevent="addToCart(prod.id)">
-            <i class="fa fa-cart-plus"></i>
-          </a>
-        </li>
-      </ul>
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="prod-cate">
+            <h4>作品分類</h4>
+            <ul>
+              <li><a href="#" :class="{active: isAll}" @click.prevent="getAll">全部</a></li>
+              <li><a href="#" :class="{active: isLand}" @click.prevent="getLand()">風景畫</a></li>
+              <li><a href="#" :class="{active: isSea}" @click.prevent="getSea()">海景畫</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm-9">
+          <ul class="row prod-list">
+            <li v-for="prod in prods" :key="prod.id" class="col-md-3 col-sm-6 col-6 full-479">
+              <router-link :to="`/product/${prod.id}`">
+                <div class="prod-image">
+                  <img :src="prod.imageUrl[0]" :alt="prod.title" />
+                </div>
+                <div class="prod-title">{{ prod.title }}</div>
+                <div
+                  class="prod-origin-price"
+                  v-if="prod.origin_price === prod.price"
+                >
+                  售價&nbsp;&nbsp;&nbsp;&nbsp;{{ prod.origin_price | currency }}
+                </div>
+                <span v-else>
+                  <div class="prod-origin-price old-price">
+                    售價&nbsp;&nbsp;&nbsp;&nbsp;
+                    {{ prod.origin_price | currency }}
+                  </div>
+                  <div class="prod-price">
+                    <b>特價&nbsp;&nbsp;&nbsp;&nbsp;{{ prod.price | currency }}</b>
+                  </div>
+                </span>
+              </router-link>
+              <a href="#" class="add-to-cart" @click.prevent="addToCart(prod.id)">
+                <i class="fa fa-cart-plus"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
       <hr />
       <div class="float-right">
@@ -54,7 +68,10 @@ export default {
     return {
       prods: [],
       isLoading: false,
-      pagination: {}
+      pagination: {},
+      isAll: true,
+      isLand: false,
+      isSea: false
     };
   },
   methods: {
@@ -128,7 +145,78 @@ export default {
         .then(res => {
           this.isLoading = false;
           this.prods = res.data.data;
+          console.log(this.prods);
           this.pagination = res.data.meta.pagination;
+        })
+        .catch(err => {
+          console.dir(err);
+          this.isLoading = false;
+          this.$bus.$emit("message:push", err.response.data.message, "danger");
+        });
+    },
+    getAll() {
+      this.getProds();
+      this.isAll = true;
+      this.isLand = false;
+      this.isSea = false;
+    },
+    getLand() {
+      this.isLoading = true;
+      this.isAll = false;
+      this.isLand = true;
+      this.isSea = false;
+      const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_UUID}/ec/products`;
+      this.$http
+        .get(url)
+        .then(res => {
+          this.isLoading = false;
+          this.prods = res.data.data;
+          console.log(res);
+          // this.prods.$set.length = 30;
+          // console.log(this.prods.length);
+          this.pagination = res.data.meta.pagination;
+          let obj = JSON.parse(JSON.stringify(this.prods));
+          // let land = JSON.parse(JSON.stringify(this.prods));
+          // let sea = JSON.parse(JSON.stringify(this.prods));
+          obj = this.prods.filter(item => {
+            // console.log(item);
+            return item.category === "風景";
+            // return item.origin_price === 4709;
+          });
+          // console.log(land);
+          this.prods = obj;
+        })
+        .catch(err => {
+          console.dir(err);
+          this.isLoading = false;
+          this.$bus.$emit("message:push", err.response.data.message, "danger");
+        });
+    },
+    getSea() {
+      this.isLoading = true;
+      this.isAll = false;
+      this.isLand = false;
+      this.isSea = true;
+      const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_UUID}/ec/products`;
+      this.$http
+        .get(url)
+        .then(res => {
+          this.isLoading = false;
+          this.prods = res.data.data;
+          console.log(res);
+          // this.prods.$set.length = 30;
+          console.log(this.prods.length);
+          this.pagination = res.data.meta.pagination;
+          let obj = JSON.parse(JSON.stringify(this.prods));
+          // let land = JSON.parse(JSON.stringify(this.prods));
+          // let sea = JSON.parse(JSON.stringify(this.prods));
+          obj = this.prods.filter(item => {
+            // console.log(item);
+            return item.category === "海景";
+            // return item.origin_price === 4709;
+          });
+          // console.log(land);
+          this.prods = obj;
         })
         .catch(err => {
           console.dir(err);
